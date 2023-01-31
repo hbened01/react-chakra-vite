696b573af9a106718183a7bd6a73e592dd777080
Humberto Benedito <humbertobenedito@gmail.com>
2023-01-31 11:29:36 +0100

    fix: Se elimina console.log

--- src/utils/mockApi.js
+++ src/utils/mockApi.js
@@ -1,6 +1,5 @@
 export const userLogin = async (data) => {
   const {email, password} = data;
-  console.log(email, password);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (email === 'humbertobenedito@gmail.com' && password === '1234') {
