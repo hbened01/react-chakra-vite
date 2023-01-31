8fdd2801919bdcaa92b072751d94f6de08ab2fa6
Humberto Benedito <humbertobenedito@gmail.com>
2023-01-31 08:48:41 +0100

    [mockApi] update validation test

--- src/utils/mockApi.js
+++ src/utils/mockApi.js
@@ -1,7 +1,7 @@
 export const userLogin = async ({ email, password }) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (email === 'test@test.com' && password === 'password') {
+      if (email === 'humbertobenedito@gmail.com' && password === '1234') {
         resolve();
       } else {
         reject();
